82f52101fab67c7902df37c7bde77d46af914187
Peter <schmidt.multimedia@gmail.com>
2023-05-24 12:42:02 +0200

    Cat/Tomcat

--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -5,15 +5,13 @@ public class Cat {
   private String name;
   private String furColor;
   private int age; 
-  private boolean isFemale;
 
   private int counter;
 
-  public Cat(String name, String furColor, int age, boolean isFemale) {
+  public Cat(String name, String furColor, int age) {
     this.name = name;
     this.furColor = furColor;
     this.age = age;
-    this.isFemale = isFemale;
   }
 
   public String getStringAttributes(String op) {
@@ -28,11 +26,7 @@ public class Cat {
   }
 
   public String getAge() {
-    if (isFemale) {
-      return checkEscalationLevel();
-    } else {
-      return String.valueOf(age);
-    }
+    return checkEscalationLevel();
   }
 
   private String checkEscalationLevel() {
